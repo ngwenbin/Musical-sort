72180376168b0d6d301465705686391fbe15f6fe
Wen Bin <wenbin_ng@yahoo.com.sg>
2022-10-24 01:00:39 +0800

    fix; fixed unused var error

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -19,7 +19,7 @@ const Select = ({
   onChangeHandler,
   ...props
 }: SelectProps) => {
-  const [selected, setSelected] = useState<string>();
+  // const [selected, setSelected] = useState<string>();
 
   return (
     <div className={clsx(labelOrient === "hor" && "flex items-center gap-x-2")}>
@@ -40,7 +40,7 @@ const Select = ({
         className="block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500"
         defaultValue={options[defaultOptIdx]}
         onChange={(e) => {
-          setSelected(e.target.value);
+          // setSelected(e.target.value);
           onChangeHandler && onChangeHandler(e.target.value);
         }}
         {...props}
