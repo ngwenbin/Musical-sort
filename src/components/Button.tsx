42a133e577845e688a886c504548aa6c1cf61760
Wen Bin <wenbin_ng@yahoo.com.sg>
2022-10-24 00:56:52 +0800

    feat: created select component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -16,7 +16,7 @@ const Button = ({
   return (
     <button
       className={clsx(
-        "inline-flex items-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium leading-4 text-blue-700 hover:bg-cyan-200 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 disabled:opacity-50",
+        "inline-flex items-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium leading-4 text-blue-700 hover:bg-blue-200 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none",
         buttonClassName
       )}
       disabled={disabled}
---
+++ src/components/Select.tsx
@@ -0,0 +1,60 @@
+import clsx from "clsx";
+import React, { useState } from "react";
+
+interface SelectProps extends React.HTMLAttributes<HTMLSelectElement> {
+  options: Array<string>;
+  defaultOptIdx?: number;
+  label?: string;
+  labelClassName?: string;
+  labelOrient?: "ver" | "hor";
+  onChangeHandler?: (value: string) => void;
+}
+
+const Select = ({
+  options,
+  defaultOptIdx = 0,
+  label,
+  labelClassName,
+  labelOrient = "ver",
+  onChangeHandler,
+  ...props
+}: SelectProps) => {
+  const [selected, setSelected] = useState<string>();
+
+  return (
+    <div className={clsx(labelOrient === "hor" && "flex items-center gap-x-2")}>
+      {label && (
+        <label
+          htmlFor="select"
+          className={clsx(
+            "block text-sm font-medium text-gray-900",
+            labelClassName
+          )}
+        >
+          {label}
+        </label>
+      )}
+      <select
+        id="select"
+        name="select"
+        className="block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500"
+        defaultValue={options[defaultOptIdx]}
+        onChange={(e) => {
+          setSelected(e.target.value);
+          onChangeHandler && onChangeHandler(e.target.value);
+        }}
+        {...props}
+      >
+        {options.map((item, key) => {
+          return (
+            <option key={key} value={item}>
+              {item}
+            </option>
+          );
+        })}
+      </select>
+    </div>
+  );
+};
+
+export default Select;
