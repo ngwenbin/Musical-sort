90305d7c99bbac7400dc67e1ed6a01d147bd35b3
Wen Bin <wenbin_ng@yahoo.com.sg>
2022-10-23 23:50:31 +0800

    feat: added Button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,30 @@
+import clsx from "clsx";
+import React, { ReactNode } from "react";
+
+interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
+  buttonClassName?: string;
+  children?: ReactNode;
+  disabled?: boolean;
+}
+
+const Button = ({
+  buttonClassName,
+  children,
+  disabled = false,
+  ...props
+}: ButtonProps) => {
+  return (
+    <button
+      className={clsx(
+        "inline-flex items-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium leading-4 text-blue-700 hover:bg-cyan-200 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 disabled:opacity-50",
+        buttonClassName
+      )}
+      disabled={disabled}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
